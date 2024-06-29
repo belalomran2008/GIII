8bcf323b1b4c51bad512f37d8a3d65f4f7774a29
Belalomran2008 <Belalomran2008@gmail.com>
2024-06-29 13:01:22 +0300

    Done keda?

--- worldwideclock.py
+++ worldwideclock.py
@@ -1,13 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
 url = requests.get("https://books.toscrape.com/")
 soup = BeautifulSoup(url.content, 'html.parser')
 article = soup.find_all('article', attrs = { "class" : "product_pod"})
+# print(article[0])
 books = []
+prices = []
+ratings = []
 for i in range(len(article)):
     name = article[i].find('h3').get_text()
+    price = article[i].find('p', attrs = { "class" : "price_color"}).get_text()
+    rating = article[i].find('p').get('class')[1]
+    prices.append(price)
     books.append(name)
+    ratings.append(rating)
 
-print(books)
+a = {'name': books,
+    'rating': ratings,
+    'price': prices}
 
+df = pd.DataFrame(a)
+print(df)
